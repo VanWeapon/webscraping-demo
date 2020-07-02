ff05a41ab56e26820d107f7c923fddcaebb3cefa
Luke Van Epen <luke.vanepen@kineticit.com.au>
2020-07-02 20:42:40 +1000

    styling

--- index.js
+++ index.js
@@ -1,52 +1,40 @@
-const puppeteer = require('puppeteer');
-let tickers = ["CBA.AX", 
-"QAN.AX", 
-"TSLA", 
-"CSL.AX", 
-"SCG.AX", 
-"NAB.AX", 
-"TLS.AX",
-"BHP.AX"
-];
+const puppeteer = require("puppeteer");
+let tickers = ["CBA.AX", "QAN.AX", "TSLA", "CSL.AX", "SCG.AX", "NAB.AX", "TLS.AX", "BHP.AX"];
 
-async function runPuppet(ticker){
+async function runPuppet(ticker) {
+	let url = "https://au.finance.yahoo.com/";
 
-    let url = "https://au.finance.yahoo.com/";
+	let browser = await puppeteer.launch({ headless: true });
+	let page = await browser.newPage();
 
-    let browser = await puppeteer.launch({headless: true});
-    let page = await browser.newPage();
-
-    await page.goto(url, { waitUntil: "networkidle2" });
+	await page.goto(url, { waitUntil: "networkidle2" });
 	await page.type("input[id='yfin-usr-qry']", ticker);
-    await page.click("button[id='search-button'");
-    await page.waitForNavigation({waitUntil: 'networkidle2'});
-    
+	await page.click("button[id='search-button'");
+	await page.waitForNavigation({ waitUntil: "networkidle2" });
 
-    let data = await page.evaluate(() => {
+	let data = await page.evaluate(() => {
+		let header = document.querySelector('div[id="quote-header-info"]');
 
-        let header = document.querySelector('div[id="quote-header-info"]');
+		let spans = header.children[2].firstChild.querySelectorAll("span");
+		let price = spans[0].innerText;
+		let movements = spans[1].innerText.split(" ");
+		let movementAmount = movements[0];
+		let movementPercent = movements[1];
 
-        let spans = header.children[2].firstChild.querySelectorAll('span');
-        let price = spans[0].innerText;
-        let movements = spans[1].innerText.split(' ');
-        let movementAmount = movements[0];
-        let movementPercent = movements[1];
+		let companyName = header.querySelector("h1").innerText;
 
-        let companyName = header.querySelector('h1').innerText;
+		return {
+			companyName,
+			price,
+			movementAmount,
+			movementPercent,
+		};
+	});
+	data.ticker = ticker;
 
-        return {
-            companyName,
-            price,
-            movementAmount,
-            movementPercent
-        };
-    });
-    data.ticker = ticker;
+	console.log(data);
 
-    console.log(data);
+	await browser.close();
+}
 
-    await browser.close();
-
-};
-
-tickers.forEach(ticker => runPuppet(ticker));
+tickers.forEach((ticker) => runPuppet(ticker));
