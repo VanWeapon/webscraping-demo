e41bd90cf867dbabf70629394b9c0fe035ced48d
Luke Van Epen <luke.vanepen@kineticit.com.au>
2020-07-02 19:53:34 +1000

    first test

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ const puppeteer = require('puppeteer');
     let browser = await puppeteer.launch();
     let page = await browser.newPage();
 
-    await page.goto(url);
+    await page.goto(url, {waitUntil: 'networkidle2'});
 
+    let data = await page.evaluate(() => {
+        let title = document.querySelector('div[class="title_wrapper"] > h1').innerText.trim();
+        let rating = document.querySelector('span[itemprop="ratingValue"]').innerText;
+        let ratingCount = document.querySelector('span[itemprop="ratingCount"]').innerText;
 
+        return {
+            title,
+            rating,
+            ratingCount
+        };
+    });
 })();
